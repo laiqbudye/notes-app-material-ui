0e18cfb537244088b6749957865cd7a5400e62bb
laiqb <laiqbudye@gmail.com>
2021-11-24 00:14:10 +0530

    mui theme override || overriden primary color & default font of mui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
+import { createTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core'
+
+const theme = createTheme({    // it will override default setting of mui
+  palette: {
+    primary: {
+      main: '#fefefe'       // here it changing primary clor of mui
+    }
+  },
+  typography: {             // here it has changed default font of mui
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
