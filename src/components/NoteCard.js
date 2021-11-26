936e431abcaaf0c04490c2280c54b8d1dd6d91c7
laiqb <laiqbudye@gmail.com>
2021-11-27 01:02:57 +0530

    Avatar || added avatar for notes

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -3,10 +3,30 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { IconButton } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
+import Avatar from '@material-ui/core/Avatar';
+import { blue, yellow, pink, green } from '@material-ui/core/colors';
 
+const useStyles = makeStyles({
+    avatar: {
+        backgroundColor: (note) => {
+            if (note.category === 'work') {
+                return yellow[700]
+            }  
+            if (note.category === 'todos') {
+                return pink[700]
+            }  
+            if (note.category === 'money') {
+                return green[700]
+            } 
+            return blue[700]
+        }
+    }
+})
 function NoteCard({ note, handleNoteDelete }) {
+    const classes = useStyles(note);
+
     return (
         <div>
             <Card elevation={1}>
@@ -15,6 +35,11 @@ function NoteCard({ note, handleNoteDelete }) {
                         <IconButton onClick={() => handleNoteDelete(note.id)}>
                             <DeleteOutlined />
                         </IconButton>
+                    }
+                    avatar={
+                        <Avatar className={classes.avatar}>
+                            {note.category[0].toUpperCase()}
+                        </Avatar>
                     }
                     title={note.title}
                     subheader={note.category}
