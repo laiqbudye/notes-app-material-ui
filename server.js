9137793b6c84ab18f6d7a12f9812283353dd95ed
laiqb <laiqbudye@gmail.com>
2021-11-28 00:20:33 +0530

    Production || adding changes required to deploy app on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const notesRoutes = require('./routes/notes');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });    // this code needs to read data from env file, otherwise it wont read data
 
@@ -12,6 +13,17 @@ const app = express();
 app.use(express.json());         // this gives req.body obj in which we get all our passed values automatically
 //mapping api with routes
 app.use('/api/notes', notesRoutes);
+
+
+// serve static asset in production
+if(process.env.NODE_ENV === 'production'){
+    //set the static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
