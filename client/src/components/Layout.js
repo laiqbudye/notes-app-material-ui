588e0adfa193c8803ad0a26305ae331303187c4a
laiqb <laiqbudye@gmail.com>
2021-11-28 06:32:11 +0530

    AppBar With SideDrawer || removed old one &  added new sidebar which will open on menu click

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
-import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import { useHistory, useLocation } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import DescriptionIcon from '@material-ui/icons/Description';
-
-const drawerWidth = 250;
+import AppBarWithSideBar from './AppBarWithSideBar';
 
 const useStyle = makeStyles((theme) => {
     return {
@@ -22,96 +9,20 @@ const useStyle = makeStyles((theme) => {
             width: '100%',
             padding: theme.spacing(3)
         },
-        title: {
-            padding: theme.spacing(3)
-        },
-        drawer: {
-            width: drawerWidth
-        },
-        drawerPaper: {
-            width: drawerWidth
-        },
         root: {
             display: 'flex'
         },
-        active: {
-            background: '#f4f4f4'
-        },
-        appbar: {
-            width: `calc(100% - ${drawerWidth}px)`
-        },
-        toolbardummy: theme.mixins.toolbar,    // here asssigned mui toolbar to dummy toolbar to match height of both so our cards wont overlap
-        description: {
-            flexGrow: 1
-        },
-        avatar: {
-            marginLeft: theme.spacing(2)
-        }
+        toolbardummy: theme.mixins.toolbar    // here asssigned mui toolbar to dummy toolbar to match height of both so our cards wont overlap
     }
 });
 
 function Layout({ children }) {
     const classes = useStyle();
-    const history = useHistory();
-    const location = useLocation();
-
-    const menuItems = [
-        {
-            text: "My Notes",
-            icon: <SubjectOutlined color='secondary' />,
-            path: '/'
-        },
-        {
-            text: "Create Note",
-            icon: <AddCircleOutlined color='secondary' />,
-            path: '/create'
-        }
-    ]
 
     return (
         <div className={classes.root}>
-            <AppBar color='secondary' className={classes.appbar}
-                elevation={0}
-            >
-                <Toolbar>
-                    <Typography
-                        variant='h5'
-                        className={classes.description}
-                    >
-                        Hey Laiq!!! Hope you are having a good day
-                    </Typography>
-                    <Typography>Laiq</Typography>
-                    <Avatar className={classes.avatar} src='./laiq.jpg'></Avatar>
-                </Toolbar>
-            </AppBar>
-            {/* side drawer */}
-            <Drawer className={classes.drawer}
-                variant='permanent'
-                anchor='left'
-                classes={{ paper: classes.drawerPaper }}
-            >
-                <div>
-                    <Typography variant='h5' className={classes.title}>
-                         <DescriptionIcon /> Web Diary
-                    </Typography>
-                </div>
 
-                {/* List items/menu items inside side drawer */}
-                <List>
-                    {menuItems.map(item => (
-                        <ListItem
-                            key={item.text}
-                            button
-                            onClick={() => history.push(item.path)}
-                            className={location.pathname === item.path ? classes.active : null}
-                        >
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text}></ListItemText>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-
+            <AppBarWithSideBar />
 
             <div className={classes.page}>
                 <div className={classes.toolbardummy}></div>
